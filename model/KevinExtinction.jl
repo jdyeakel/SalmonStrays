d0e562243c81bbf3e8ef031fdd8075db7a97bc86
jdyeakel <jdyeakel@gmail.com>
2017-05-27 08:58:14 -0700

    msv3 and fig:relax changes

--- model/KevinExtinction.jl
+++ model/KevinExtinction.jl
@@ -381,3 +381,72 @@ for (i in 1:length($mvec)) {
 }
 dev.off()
 """
+
+
+namespace = string("$(homedir())/Dropbox/PostDoc/2017_SalmonStrays/manuscript/figs2/fig_relax_lowh.pdf");
+R"""
+library(RColorBrewer)
+pdf($namespace,height=4,width=10)
+pal = brewer.pal(9,'Greys')
+palsub = pal[c(4,6,8)];
+par(mfrow=c(1,2),mai = c(0.8, 0.8, 0.1, 0.1))
+plot($mvec,$(ma_rth2[:,1]),col=palsub[1],type='l',log='y',cex=0.5,lwd=2,xlab='m',ylab='Recovery time',ylim=c(min($rt),max($(ma_rth2))))
+lines($mvec,$(ma_rth2[:,2]),col=palsub[2],cex=0.5,lwd=2)
+lines($mvec,$(ma_rth2[:,3]),col=palsub[3],cex=0.5,lwd=2)
+text(par('usr')[1]-0.09,max($(ma_rth2)),'(a)', xpd=TRUE)
+
+
+
+plot($(ma1_m2_ddm[:,1]),$(ma_rth2_ddm[:,1]),col=palsub[1],log='y',cex=0.5,pch=16,xlab='m*',ylab='Recovery time',ylim=c(min($rt_ddm),max($(ma_rth2_ddm))))
+points($(ma2_m2_ddm[:,1]),$(ma_rth2_ddm[:,1]),col=palsub[1],cex=0.5,pch=16)
+points($(ma1_m2_ddm[:,2]),$(ma_rth2_ddm[:,2]),col=palsub[2],cex=0.5,pch=16)
+points($(ma2_m2_ddm[:,2]),$(ma_rth2_ddm[:,2]),col=palsub[2],cex=0.5,pch=16)
+points($(ma1_m2_ddm[:,3]),$(ma_rth2_ddm[:,3]),col=palsub[3],cex=0.5,pch=16)
+points($(ma2_m2_ddm[:,3]),$(ma_rth2_ddm[:,3]),col=palsub[3],cex=0.5,pch=16)
+#text(0.01,max($(ma_rth2_ddm)),expression(paste(h^2,'=0.2')))
+for (i in 1:length($mvec)) {
+  segments($(ma1_m2_ddm[:,1])[i],$(ma_rth2_ddm[:,1])[i],$(ma2_m2_ddm[:,1])[i],$(ma_rth2_ddm[:,1])[i],col=palsub[1])
+  segments($(ma1_m2_ddm[:,2])[i],$(ma_rth2_ddm[:,2])[i],$(ma2_m2_ddm[:,2])[i],$(ma_rth2_ddm[:,2])[i],col=palsub[2])
+  segments($(ma1_m2_ddm[:,3])[i],$(ma_rth2_ddm[:,3])[i],$(ma2_m2_ddm[:,3])[i],$(ma_rth2_ddm[:,3])[i],col=palsub[3])
+}
+text(-0.072,max($(ma_rth2_ddm)),'(b)', xpd=TRUE)
+types = c('subordinate poulation extinct','dominant population extinct','near-collapse')
+legend(x=0.12,y=130,legend=types,col=palsub,pch=22,xpd=TRUE,pt.bg=palsub,cex=1, bty="n") #,title=expression(paste(Delta,theta))
+
+dev.off()
+"""
+
+
+
+namespace = string("$(homedir())/Dropbox/PostDoc/2017_SalmonStrays/manuscript/figs2/fig_relax_highh.pdf");
+R"""
+library(RColorBrewer)
+pdf($namespace,height=4,width=10)
+pal = brewer.pal(9,'Greys')
+palsub = pal[c(4,6,8)];
+par(mfrow=c(1,2),mai = c(0.8, 0.8, 0.1, 0.1))
+
+plot($mvec,$(ma_rth8[:,1]),col=palsub[1],type='l',log='y',cex=0.5,lwd=2,xlab='m',ylab='Recovery time',ylim=c(min($rt),max($(ma_rth8))))
+lines($mvec,$(ma_rth8[:,2]),col=palsub[2],cex=0.5,lwd=2)
+lines($mvec,$(ma_rth8[:,3]),col=palsub[3],cex=0.5,lwd=2)
+text(par('usr')[1]-0.085,max($(ma_rth8))+500,'(a)', xpd=TRUE)
+types = c('subordinate poulation ext','dominant population ext','near-collapse')
+legend(x=0.2,y=3500,legend=types,col=palsub,pch=22,xpd=TRUE,pt.bg=palsub,cex=1, bty="n") #,title=expression(paste(Delta,theta))
+
+
+plot($(ma1_m8_ddm[:,1]),$(ma_rth8_ddm[:,1]),col=palsub[1],log='y',cex=0.5,pch=16,xlab='m*',ylab='Recovery time',ylim=c(min($rt_ddm),max($(ma_rth8_ddm))))
+points($(ma2_m8_ddm[:,1]),$(ma_rth8_ddm[:,1]),col=palsub[1],cex=0.5,pch=16)
+points($(ma1_m8_ddm[:,2]),$(ma_rth8_ddm[:,2]),col=palsub[2],cex=0.5,pch=16)
+points($(ma2_m8_ddm[:,2]),$(ma_rth8_ddm[:,2]),col=palsub[2],cex=0.5,pch=16)
+points($(ma1_m8_ddm[:,3]),$(ma_rth8_ddm[:,3]),col=palsub[3],cex=0.5,pch=16)
+points($(ma2_m8_ddm[:,3]),$(ma_rth8_ddm[:,3]),col=palsub[3],cex=0.5,pch=16)
+text(-0.072,max($(ma_rth8_ddm))+150,'(b)', xpd=TRUE)
+#text(0.01,max($(ma_rth8_ddm)),expression(paste(h^2,'=0.8')))
+for (i in 1:length($mvec)) {
+  segments($(ma1_m8_ddm[:,1])[i],$(ma_rth8_ddm[:,1])[i],$(ma2_m8_ddm[:,1])[i],$(ma_rth8_ddm[:,1])[i],col=palsub[1])
+  segments($(ma1_m8_ddm[:,2])[i],$(ma_rth8_ddm[:,2])[i],$(ma2_m8_ddm[:,2])[i],$(ma_rth8_ddm[:,2])[i],col=palsub[2])
+  segments($(ma1_m8_ddm[:,3])[i],$(ma_rth8_ddm[:,3])[i],$(ma2_m8_ddm[:,3])[i],$(ma_rth8_ddm[:,3])[i],col=palsub[3])
+}
+     
+dev.off()
+"""
